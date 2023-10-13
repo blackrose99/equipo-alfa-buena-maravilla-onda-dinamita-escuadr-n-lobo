826b1320e988afde34481b2c4b95f5683afa6644
Maicol <maicolcubidespe@gmail.com>
2023-10-13 10:03:57 -0500

    hover en js

--- js/animaciones.js
+++ js/animaciones.js
@@ -1,1 +1,18 @@
 
+document.addEventListener('DOMContentLoaded', function(){
+    const imagenes = document.querySelectorAll('.image img');
+
+    imagenes.forEach(function(image){
+        image.addEventListener('mouseover', function(){
+            image.style.transform = 'scale(1.1)';
+            image.style.zIndex = '1';
+        });
+
+        image.addEventListener('mouseout', function(){
+            image.style.transform = 'scale(1)';
+            image.style.zIndex = '0';
+        });
+
+    });
+});
+
