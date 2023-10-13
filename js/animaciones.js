1683d248132e10e863ba2762ec60b562e12223fe
Juan David <juandavidpinvilla@gmail.com>
2023-10-13 10:07:32 -0500

    ultimas arreglos

--- js/animaciones.js
+++ js/animaciones.js
@@ -1,1 +1,16 @@
+document.addEventListener("DOMContentLoaded", function(){
+    var btnEnviar = document.getElementById("btn-form");
 
+    btnEnviar.addEventListener("click", function (){
+        var nombre = document.getElementById("nombre").value.trim();
+        var email = document.getElementById("email").value.trim();
+        var mensaje = document.getElementById("mensaje").value.trim();
+
+        if(nombre === "" || email === "" || mensaje === ""){
+            alert("Por favor, rellene todos los campos.");
+        } else {
+            alert("Datos enviados correctamente");
+            window.location.href = "uts.html";
+        }
+    });
+});
